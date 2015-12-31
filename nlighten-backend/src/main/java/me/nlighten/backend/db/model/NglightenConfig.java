a9032528d6ef71381973ffbb18aca3bf02a4ee33
lubo3 <korenko.lubo@gmail.com>
2015-12-31 13:27:10 +0100

    initial commit to open a discussion

---
+++ nlighten-backend/src/main/java/me/nlighten/backend/db/model/NglightenConfig.java
@@ -0,0 +1,15 @@
+package me.nlighten.backend.db.model;
+
+public class NglightenConfig {
+  
+  private String cdiDebugProperty;
+
+  public String getCdiDebugProperty() {
+    return cdiDebugProperty;
+  }
+
+  public void setCdiDebugProperty(String cdiDebugProperty) {
+    this.cdiDebugProperty = cdiDebugProperty;
+  }
+  
+}
