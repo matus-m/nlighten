fd673220a9afb37187892280fe3477c93206e587
Martin <Martin@Gajdos.Email>
2016-02-10 22:40:12 +0100

    Add I18n feature

---
+++ nlighten-backend/src/main/java/me/nlighten/backend/common/util/DSA.java
@@ -0,0 +1,24 @@
+package me.nlighten.backend.common.util;
+
+import java.io.FileNotFoundException;
+
+public class DSA {
+  public static void main(String[] args) throws FileNotFoundException {
+    String[] a = "a=b".split("=", 2);
+    System.out.println("");
+
+    // FileReader f = new FileReader(ClassLoader.getSystemClassLoader()
+    // .getResource(
+    // "translations/me.nlighten.backend.test.resource.FQNFileNameTranslations_en.properties")
+    // .getFile());
+  }
+
+  private static void a(String s) {
+
+  }
+
+  enum B {
+    A
+  }
+
+}
