2f8f04cbddafaa623d5bbe84a7ea2a46a018e074
Martin <Martin@Gajdos.Email>
2016-02-13 15:09:35 +0100

    Fix #6
    Added documentation
    Exception handling
    Negative test
    Code optimization

--- nlighten-backend/src/test/java/me/nlighten/backend/test/TranslationUtilTest.java
+++ nlighten-backend/src/test/java/me/nlighten/backend/test/TranslationUtilTest.java
@@ -1,11 +1,13 @@
 package me.nlighten.backend.test;
 
 import java.util.Locale;
+import java.util.MissingResourceException;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import me.nlighten.backend.common.util.I18nUtil;
+import me.nlighten.backend.test.resource.NoTranslationsEnum;
 import me.nlighten.backend.test.resource.TestTranslations;
 
 
@@ -18,7 +20,8 @@ import me.nlighten.backend.test.resource.TestTranslations;
 public class TranslationUtilTest {
 
   @Test
-  public void simpleTranslationTest() {
+  public void simpleTranslationTest() throws Exception {
+    System.setProperty("FULL_PATH_TRANSLATIONS", "true");
     Assert.assertEquals("Übergut koolaine Buchstaben",
         I18nUtil.translate(TestTranslations.SOME_COOL_TEXT, new Locale("de")));
     Assert.assertEquals("Some cool text",
@@ -26,11 +29,17 @@ public class TranslationUtilTest {
   }
 
   @Test
-  public void translationWithArgumentsTest() {
+  public void translationWithArgumentsTest() throws Exception {
+    System.setProperty("FULL_PATH_TRANSLATIONS", "true");
     Assert.assertEquals("Nummer ?Streit? 1: 22 String ?Streit? 2: mein Auto", I18nUtil
         .translate(TestTranslations.TEXT_WITH_ARGUMENTS, new Locale("de"), 22, "mein Auto"));
     Assert.assertEquals("Number Argument 1: 341 String Argument 2: Rapture's comming!",
         I18nUtil.translate(TestTranslations.TEXT_WITH_ARGUMENTS, new Locale("en"), 341,
             "Rapture's comming!"));
+  }
+
+  @Test(expected = MissingResourceException.class)
+  public void noTranslationsTest() throws Exception {
+    I18nUtil.translate(NoTranslationsEnum.TRANSLATIONS, new Locale("de"));
   }
 }
