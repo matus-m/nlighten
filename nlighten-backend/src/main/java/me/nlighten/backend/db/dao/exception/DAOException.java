8e83226dca69ecca94331cc1dc4f3f55cbd435b6
lubo3 <korenko.lubo@gmail.com>
2016-03-14 15:38:47 +0100

    fix for DAOException hides cause bug
    fix build exception

--- nlighten-backend/src/main/java/me/nlighten/backend/db/dao/exception/DAOException.java
+++ nlighten-backend/src/main/java/me/nlighten/backend/db/dao/exception/DAOException.java
@@ -30,13 +30,6 @@ public class DAOException extends Exception {
   }
 
   /**
-   * @param message
-   */
-  public DAOException(String message) {
-    super(message);
-  }
-
-  /**
    * @param cause
    */
   public DAOException(Throwable cause) {
@@ -44,33 +37,16 @@ public class DAOException extends Exception {
   }
 
   /**
-   * @param message
-   * @param cause
-   */
-  public DAOException(String message, Throwable cause) {
-    super(message, cause);
-  }
-
-  /**
    * Instantiates a new DAO exception.
    *
+   * @param cause the cause
    * @param messageKey the message key
    * @param arguments the arguments
    */
-  public DAOException(Enum<?> messageKey, Object... arguments) {
+  public DAOException(Throwable cause, Enum<?> messageKey, Object... arguments) {
+    super(cause);
     this.messageKey = messageKey;
     this.arguments = arguments;
-  }
-
-  /**
-   * @param message the message
-   * @param cause the cause
-   * @param enableSuppression the enable suppression
-   * @param writableStackTrace the writable stack trace
-   */
-  protected DAOException(String message, Throwable cause, boolean enableSuppression,
-      boolean writableStackTrace) {
-    super(message, cause, enableSuppression, writableStackTrace);
   }
 
   public Enum<?> getMessageKey() {
